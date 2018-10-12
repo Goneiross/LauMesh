8f7a3ae0818e00e1aa2ad45ea08dee371d852ab3
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-10-12 09:54:50 +0200

    Add I2C test

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -18,13 +19,20 @@
 
 #define PIN_I2C_MASTER_DATA 18
 #define PIN_I2C_MASTER_CLOCK 17
+#define MASTER_PORT 0
 
 #define PIN_I2C_SLAVE1_DATA 2
 #define PIN_I2C_SLAVE1_CLOCK 4
 #define I2C_SLAVE1_ADRESS 0
+#define SLAVE1_PORT 0
 
-#define T_TIMEOUT 1000
+#define I2C_RECEVING_BUFFER_SIZE 0
+#define I2C_SENDING_BUFFER_SIZE 0
+#define I2C_DATA_LENGTH 1024
+
+#define i2C_TIMEOUT 1000
 #define CLOCK_SPEED 1000
+
 
 void I2cMasterInit(){
     i2c_config_t i2cMasterConfig;
@@ -34,6 +42,9 @@ void I2cMasterInit(){
     i2cMasterConfig.scl_io_num = PIN_I2C_MASTER_CLOCK;
     i2cMasterConfig.scl_pullup_en = true;
     i2cMasterConfig.master.clk_speed = CLOCK_SPEED;
+    i2c_driver_install(MASTER_PORT,I2C_MODE_MASTER,I2C_RECEVING_BUFFER_SIZE,I2C_SENDING_BUFFER_SIZE,0);
+    i2c_param_config(MASTER_PORT,&i2cMasterConfig);
+    i2c_set_timeout(MASTER_PORT,i2C_TIMEOUT);
 }
 
 void I2CSalve1Init(){
@@ -45,10 +56,19 @@ void I2CSalve1Init(){
     i2cSlaveConfig.scl_pullup_en = true;
     i2cSlaveConfig.slave.addr_10bit_en = true;
     i2cSlaveConfig.slave.slave_addr = I2C_SLAVE1_ADRESS;
+    i2c_driver_install(MASTER_PORT,I2C_MODE_SLAVE,I2C_RECEVING_BUFFER_SIZE,I2C_SENDING_BUFFER_SIZE,0);
 }
 
 int I2cTest(void* pvParameters){
+    uint8_t data_read = 0 ;
+    //esp_err_t testRead;
     printf("Teste de la connection I2C \n");
+    i2c_cmd_handle_t testCmd = i2c_cmd_link_create();
+    i2c_master_start(testCmd);
+    i2c_master_read_byte(testCmd,I2C_SLAVE1_ADRESS,0);
+    i2c_master_read(testCmd,data_read,I2C_DATA_LENGTH,0);
+    i2c_master_stop(testCmd);
+    i2c_cmd_link_delete(testCmd);
     return(0);
 }
 
