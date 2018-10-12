c7904f1838adac48da4118dfc21d64786a3fed22
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-10-12 16:01:27 +0200

    Debug multithreading and tasks

--- main/main.c
+++ main/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -21,8 +20,8 @@
 #define PIN_I2C_MASTER_CLOCK 17
 #define MASTER_PORT 0
 
-#define PIN_I2C_SLAVE1_DATA 2
-#define PIN_I2C_SLAVE1_CLOCK 4
+#define PIN_I2C_SLAVE1_DATA 21
+#define PIN_I2C_SLAVE1_CLOCK 22
 #define I2C_SLAVE1_ADRESS 0
 #define SLAVE1_PORT 0
 
@@ -31,10 +30,21 @@
 #define I2C_DATA_LENGTH 1024
 
 #define i2C_TIMEOUT 1000
-#define CLOCK_SPEED 1000
+#define CLOCK_SPEED 1000 
 
+void test(void* pvParamters){
+    time_t time0;
+    while(1){
+        time0 = time(NULL);
+        printf("En cours d'execution depuis %ld minutes\n",time0);
+        vTaskDelay(60000/portTICK_RATE_MS);
+        esp_restart();
+    }  
+    vTaskDelete(NULL);
+}
 
-void I2cMasterInit(){
+void I2cMasterInit(void* pvParameters){
+    printf("Master init");
     i2c_config_t i2cMasterConfig;
     i2cMasterConfig.mode = I2C_MODE_MASTER;
     i2cMasterConfig.sda_io_num = PIN_I2C_MASTER_DATA;
@@ -45,9 +55,11 @@ void I2cMasterInit(){
     i2c_driver_install(MASTER_PORT,I2C_MODE_MASTER,I2C_RECEVING_BUFFER_SIZE,I2C_SENDING_BUFFER_SIZE,0);
     i2c_param_config(MASTER_PORT,&i2cMasterConfig);
     i2c_set_timeout(MASTER_PORT,i2C_TIMEOUT);
+    printf("Master Init Done");
+    vTaskDelete(NULL);
 }
 
-void I2CSalve1Init(){
+void I2CSlave1Init(void* pvParameters){
     i2c_config_t i2cSlaveConfig;
     i2cSlaveConfig.mode = I2C_MODE_SLAVE;
     i2cSlaveConfig.sda_io_num = PIN_I2C_SLAVE1_DATA;
@@ -57,9 +69,11 @@ void I2CSalve1Init(){
     i2cSlaveConfig.slave.addr_10bit_en = true;
     i2cSlaveConfig.slave.slave_addr = I2C_SLAVE1_ADRESS;
     i2c_driver_install(MASTER_PORT,I2C_MODE_SLAVE,I2C_RECEVING_BUFFER_SIZE,I2C_SENDING_BUFFER_SIZE,0);
+    printf("Slave init done");
+    vTaskDelete(NULL);
 }
 
-int I2cTest(void* pvParameters){
+void I2cTest(void* pvParameters){
     uint8_t data_read = 0 ;
     //esp_err_t testRead;
     printf("Teste de la connection I2C \n");
@@ -69,12 +83,13 @@ int I2cTest(void* pvParameters){
     i2c_master_read(testCmd,data_read,I2C_DATA_LENGTH,0);
     i2c_master_stop(testCmd);
     i2c_cmd_link_delete(testCmd);
-    return(0);
+    printf('Test done');
+    vTaskDelete(NULL);
 }
-
 void app_main(){
     printf("Initialisation en cours ... \n");
-    xTaskCreatePinnedToCore(&I2cMasterInit,"I2cMasterInit",NULL,128,0,NULL,0);
-    xTaskCreatePinnedToCore(&I2CSalve1Init,"I2CSalve1Init",NULL,128,0,NULL,1);
-    xTaskCreatePinnedToCore(&I2cTest,"I2Ctest",NULL,128,0,NULL,1);
+    xTaskCreatePinnedToCore(&test,"test",2048,NULL,3,NULL,0);
+    xTaskCreatePinnedToCore(&I2cMasterInit,"I2cMasterInit",2048,NULL,4,NULL,0);
+    //xTaskCreatePinnedToCore(&I2CSalve1Init,"I2CSlave1Init",1024,NULL,0,NULL,1);
+    //xTaskCreatePinnedToCore(&I2cTest,"I2Ctest",1024,NULL,0,NULL,1);
 }
