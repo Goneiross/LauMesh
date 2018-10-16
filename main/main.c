708a3948761de3f5f4f24422acea11cc24449b7d
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-10-16 09:31:40 +0200

    debug

--- main/main.c
+++ main/main.c
@@ -80,10 +80,6 @@ void LoRaConf(){
     if (done != 0) {printf("LoRaConfig ERROR");}
 }
 
-void LoRaRead(){
-    
-}
-
 void app_main(){
     printf("Initialisation en cours ... \n");
     xTaskCreatePinnedToCore(&test,"test",2048,NULL,0,NULL,0);
